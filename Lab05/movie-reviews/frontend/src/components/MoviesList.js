08bf912cfc19e9ab52fa291868a776dee7e85840
cogramer <hausmauswow@gmail.com>
2026-05-03 21:46:35 +0700

    hoàn thành lab05

---
+++ Lab05/movie-reviews/frontend/src/components/MoviesList.js
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Row, Col, Form, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchRating, setSearchRating] = useState("All Ratings");
+  const [ratings, setRatings] = useState([]);
+
+  useEffect(() => {
+    retrieveMovies();
+    retrieveRatings();
+  }, []);
+
+  const onChangeSearchTitle = (e) => {
+    setSearchTitle(e.target.value);
+  };
+
+  const onChangeSearchRating = (e) => {
+    setSearchRating(e.target.value);
+  };
+
+  const retrieveMovies = () => {
+    MovieDataService.getAll()
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const retrieveRatings = () => {
+    MovieDataService.getRatings()
+      .then((response) => {
+        setRatings(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const find = (query, by) => {
+    MovieDataService.find(query, by)
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const findByTitle = () => {
+    find(searchTitle, "title");
+  };
+
+  const findByRating = () => {
+    if (searchRating === "All Ratings") {
+      retrieveMovies();
+    } else {
+      find(searchRating, "rated");
+    }
+  };
+
+  return (
+    <div className="container mt-3">
+      <h2>Movies List</h2>
+
+      {/* SEARCH FORM */}
+      <Row className="mb-3">
+        {/* Search by Title */}
+        <Col md={6}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="Search by title"
+              value={searchTitle}
+              onChange={onChangeSearchTitle}
+            />
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="button"
+            onClick={findByTitle}
+            className="mt-2"
+          >
+            Search
+          </Button>
+        </Col>
+
+        {/* Search by Rating */}
+        <Col md={6}>
+          <Form.Group>
+            <Form.Select
+              onChange={onChangeSearchRating}
+              value={searchRating}
+            >
+              <option>All Ratings</option>
+              {ratings.map((rating, index) => (
+                <option key={index} value={rating}>
+                  {rating}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="button"
+            onClick={findByRating}
+            className="mt-2"
+          >
+            Search
+          </Button>
+        </Col>
+      </Row>
+
+      {/* MOVIES LIST */}
+      <Row>
+        {movies &&
+          movies.map((movie) => (
+            <Col key={movie._id} md={4} className="mb-4">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={movie.poster ? movie.poster + "/100px180" : ""}
+                />
+
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+
+                  <Card.Text>
+                    <strong>Rating:</strong> {movie.rated}
+                  </Card.Text>
+
+                  <Card.Text>
+                    {movie.plot}
+                  </Card.Text>
+
+                  <Link to={`/movies/${movie._id}`}>
+                    View Reviews
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </div>
+  );
+};
+
+export default MoviesList;
